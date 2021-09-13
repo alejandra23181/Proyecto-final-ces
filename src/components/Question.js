dac9e373fabd486dae208a2c14816a69aee36a57
Maria A <maria_montoya23181@elpoli.edu.co>
2021-09-13 17:30:37 -0500

    Final

--- src/components/Question.js
+++ src/components/Question.js
@@ -74,15 +74,6 @@ const Question = ({
         <div className="controls">
           <Button
             variant="contained"
-            color="secondary"
-            size="large"
-            style={{ width: 185 }}
-            onClick={handleFin}
-          >
-            Terminar partida
-          </Button>
-          <Button
-            variant="contained"
             color="primary"
             size="large"
             style={{ width: 180 }}
